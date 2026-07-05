3729591b6af160116418685e51934f15cec5e21f
rashi1m <rashipanchal2003@gmail.com>
2026-07-05 21:05:39 +0530

    Dsacommit

--- src/com/coreconcept/java/ConditionsAndLoops.java
+++ src/com/coreconcept/java/ConditionsAndLoops.java
@@ -1,10 +1,23 @@
 package com.coreconcept.java;
 
+import java.util.Scanner;
+
+
 public class ConditionsAndLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name here ");
+		String name = sc.nextLine();
+		System.out.println("My name is "+name);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
