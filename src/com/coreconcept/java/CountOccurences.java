f58e7088d4ef3ba98e1c994936aabef108317618
rashi1m <rashipanchal2003@gmail.com>
2026-07-18 20:17:58 +0530

    countOccurences2

--- src/com/coreconcept/java/CountOccurences.java
+++ src/com/coreconcept/java/CountOccurences.java
@@ -1,5 +1,22 @@
 package com.coreconcept.java;
-
+import java.util.Scanner;
 public class CountOccurences {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n = sc.nextInt();
+		int count = 0;
+		while (n > 0) {
+		    int remainder = n%10;
+		    if(remainder == 5) {
+		    	count++;
+		    }
+			
+			n= n/10;
+			
+		}
+		
+		
+	}
 }
